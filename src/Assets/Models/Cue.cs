113fc4b769fe97b9f5f73c23e50ace3c783f2ea6
Kerem Cömert <kerem0comert@gmail.com>
2022-05-13 00:43:41 +0200

    Create Cue models

---
+++ src/Assets/Models/Cue.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cue : MonoBehaviour
+{
+    private string id;
+    private string displayName;
+    private Vector3 position;
+    private Transform transformation;
+    private float displayDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
