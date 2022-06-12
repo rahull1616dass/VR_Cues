64f64eb3732bbc9320cd3abf840fcd5eff45cd9e
Kerem Cömert <kerem0comert@gmail.com>
2022-06-12 11:24:05 +0200

    Create Poll Cue fields

---
+++ src/Assets/ScriptableObject/Class/Poll/Poll.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cues.Data
+{
+    public class Poll : Cue
+    {
+        private readonly PollInfo pollInfo;
+        private readonly List<Question> questions;
+    }
+}
---
+++ src/Assets/ScriptableObject/Class/Poll/PollInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public record PollInfo
+{
+    private readonly string introText;
+    private readonly string outroText;
+}
+
