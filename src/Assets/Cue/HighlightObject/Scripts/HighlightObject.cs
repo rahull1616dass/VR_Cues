a92e750cc67a6cd1b1e57ec662a342537ef65a6e
Rahul <rahull1616dass@gmail.com>
2022-08-12 17:01:44 +0200

    Updated highlight cue

--- src/Assets/Cue/HighlightObject/Scripts/HighlightObject.cs
+++ src/Assets/Cue/HighlightObject/Scripts/HighlightObject.cs
@@ -2,45 +2,46 @@
 
 [RequireComponent(typeof(MeshRenderer))]
 public class HighlightObject : MonoBehaviour {
-  public float animationTime = 1f;
-  //public float threshold = 10.5f;
 
-  private HighlightController controller;
-  private Material material;
-  private Color normalColor;
-  private Color selectedColor;
+    public int objectID;
+    private float animationTime = 1f;
+    //public float threshold = 10.5f;
 
-  private void Awake() {
-    material = GetComponent<MeshRenderer>().material;
-    controller = FindObjectOfType<HighlightController>();
+    private HighlightController controller;
+    private Material material;
+    private Color normalColor;
+    private Color selectedColor;
 
-    normalColor = material.color;
-    selectedColor = new Color(
-      Mathf.Clamp01(200),
-      Mathf.Clamp01(200),
-      Mathf.Clamp01(200)
-    );
-  }
 
-  private void Start() {
-    //StartHighlight();
-  }
+    public void StartHighlight(Color selectedColorArg, float animationTimeArg = 1f)
+    {
+        material = GetComponent<MeshRenderer>().material;
+        controller = FindObjectOfType<HighlightController>();
 
-  public void StartHighlight() {
-    iTween.ColorTo(gameObject, iTween.Hash(
-      "color", selectedColor,
-      "time", animationTime,
-      "easetype", iTween.EaseType.linear,
-      "looptype", iTween.LoopType.pingPong
-    ));
-  }
+        normalColor = material.color;
+        selectedColor = selectedColorArg;
+        animationTime = animationTimeArg;
 
-  public void StopHighlight() {
-    iTween.Stop(gameObject);
-    material.color = normalColor;
-  }
+        
+    }
 
-  private void OnMouseDown() {
-    controller.SelectObject(this);
-  }
+
+
+    public void StartHighlight() {
+        iTween.ColorTo(gameObject, iTween.Hash(
+            "color", selectedColor,
+            "time", animationTime,
+            "easetype", iTween.EaseType.linear,
+            "looptype", iTween.LoopType.pingPong
+        ));
+    }
+
+    public void StopHighlight() {
+        iTween.Stop(gameObject);
+        material.color = normalColor;
+    }
+
+    private void OnMouseDown() {
+        controller.SelectObject(this);
+    }
 }
