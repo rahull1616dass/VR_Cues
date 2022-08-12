770c16a14a242b2dc7928fbd0be1e68620ff2e29
Kerem Cömert <kerem0comert@gmail.com>
2022-08-12 21:32:47 +0200

    Create infobox classes

---
+++ src/Assets/Cue/InfoBox/Button.cs
@@ -0,0 +1,8 @@
+
+public class Button 
+{
+    public string text { get; set; }
+    public string textColor { get; set; }
+    public string buttonBackgroundColor { get; set; }
+
+}
---
+++ src/Assets/Cue/InfoBox/InfoBox.cs
@@ -0,0 +1,13 @@
+
+public class InfoBox : Cue
+{
+    public string title { get; set; }
+    public string description { get; set; }
+    public Button[] buttons { get; set; }
+
+
+    public override void generate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
