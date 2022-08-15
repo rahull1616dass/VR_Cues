66ced6faef2789fd5f9847f80360f3a60d843c10
Kerem Cömert <kerem0comert@gmail.com>
2022-08-15 12:12:12 +0200

    Enable nullable types and add Audio fields

--- src/Assets/Cue/Audio/Audio.cs
+++ src/Assets/Cue/Audio/Audio.cs
@@ -3,6 +3,8 @@
 public class Audio : Cue
 {
     public string referenceId { get; set; }
+    public bool shouldLoop { get; set; }
+    public CueTransform? cueTransform { get; set; }
 
     public override void generate()
     {
