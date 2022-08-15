535433c0ef47c5ae0637adc3f32e762a7e0e891c
Rahul <rahull1616dass@gmail.com>
2022-08-15 15:13:18 +0200

    updated with changes

--- src/Assets/Cue/InfoBox/InfoBoxCreator.cs
+++ src/Assets/Cue/InfoBox/InfoBoxCreator.cs
@@ -8,6 +8,7 @@ public class InfoBoxCreator : MonoBehaviour
     [SerializeField] private TextMeshProUGUI[] buttonText;
     [SerializeField] private UnityEngine.UI.Image[] buttonImage;
     [SerializeField] private TextMeshProUGUI titleText, descriptionText;
+    [SerializeField] private UnityEngine.UI.Image infoImage;
     public void CreateInfoBox(InfoBox infoBox)
     {
         
