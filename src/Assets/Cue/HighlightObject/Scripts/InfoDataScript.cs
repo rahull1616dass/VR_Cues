b1ea42c8f91a992476b1216bc8cde730744a2c1c
Rahul <rahull1616dass@gmail.com>
2022-08-15 12:35:53 +0200

    Created infoDataScript

---
+++ src/Assets/Cue/HighlightObject/Scripts/InfoDataScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoDataScript : MonoBehaviour
+{
+    public void CreateInfoData(InfoBox infoData)
+    {
+
+    }
+}
