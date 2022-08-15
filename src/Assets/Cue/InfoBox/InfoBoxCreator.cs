aa179f99395cee01805a37d2b679dcc6c4f8851e
Rahul <rahull1616dass@gmail.com>
2022-08-15 12:47:26 +0200

    updated with field

--- src/Assets/Cue/InfoBox/InfoBoxCreator.cs
+++ src/Assets/Cue/InfoBox/InfoBoxCreator.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InfoBoxCreator : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI[] buttonText;
+    [SerializeField] private Color[] buttonColors;
+    [SerializeField] private TextMeshProUGUI titleText, descriptionText;
+    [SerializeField] private Color textColor;
     public void CreateInfoBox(InfoBox infoBox)
     {
         
