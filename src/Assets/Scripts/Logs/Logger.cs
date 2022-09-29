ac59bf8aaed5d5b1316ffe9c721dd1e4f588a682
Kerem Cömert <kerem0comert@gmail.com>
2022-09-29 08:02:16 +0200

    Add Logger class file

---
+++ src/Assets/Scripts/Logs/Logger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logger 
+{
+    private float startTimeOffset { get; set; } = 0;
+    private float endTimeOffset { get; set; }
+    private bool relevantForMeasurementEngine { get; set; }
+    private string tag { get; set; }
+
+}
